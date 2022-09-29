cb4785eeb60b3771c3869ba2778657e4308bc52f
jawad karim <jawadkarim121@yahoo.com>
2022-09-29 14:59:36 -0400

    committed git_project2

---
+++ git_project2/src/git_project2_pack/B.java
@@ -0,0 +1,12 @@
+package git_project2_pack;
+
+public class B {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("this is new modification for the project");
+
+	}
+
+}
---
+++ git_project2/src/git_project2_pack/C.java
@@ -0,0 +1,12 @@
+package git_project2_pack;
+
+public class C {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("this is C class");
+
+	}
+
+}
